a76f437251891c0a2bd6ca22f6a401ca2fee1189
Luc Burns <burnsluc2@gmail.com>
2021-03-10 13:46:55 -0500

    Commented and formatted

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,19 +4,16 @@ $(document).foundation();
 // Query the DOM
 let drinkBtn = $(".button");
 
-
-
 // Function to handle the button clicks
 function handleButtonClick(event) {
-    // Sets the choice to the text of the button clicked
-    let choice = event.target.textContent.toLowerCase();
-    // Sets the window location to pass the choice as a paramter
-    window.location = `./results.html?alcohol=${choice}`;
+  // Sets the choice to the text of the button clicked
+  let choice = event.target.textContent.toLowerCase();
+  // Sets the window location to pass the choice as a paramter
+  window.location = `./results.html?alcohol=${choice}`;
 }
 
 // Runs the function on the button click
 drinkBtn.on("click", (e) => {
-    localStorage.removeItem("drinks");
-    handleButtonClick(e);
-    
-})
+  localStorage.removeItem("drinks");
+  handleButtonClick(e);
+});
